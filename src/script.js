bae5c5f7067fcdeca14b3c0453870ecf37e5934b
ChakradhwajB <chakrab.nor@gmail.com>
2025-04-18 10:47:04 -0700

    Added image switching
    Implemented image switching

--- src/script.js
+++ src/script.js
@@ -1,1 +1,26 @@
-// No JavaScript needed for the simplified mobile menu
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const mobileMenuPanel = document.getElementById("mobile-menu-panel");
+const closeMobileMenu = document.getElementById("close-mobile-menu");
+if (hamburgerBtn && mobileMenuPanel && closeMobileMenu) {
+  hamburgerBtn.addEventListener("click", () => {
+    mobileMenuPanel.classList.remove("translate-x-full");
+  });
+  closeMobileMenu.addEventListener("click", () => {
+    mobileMenuPanel.classList.add("translate-x-full");
+  });
+}
+
+var myImage = document.getElementById("imageID");
+
+function firstImage() {
+  myImage.src = "Images/spotify-camp-nou.png";
+}
+function secondImage() {
+  myImage.src = "Images/Nou-Palau-Blaugrana.png";
+}
+function thirdImage() {
+  myImage.src = "Images/campus-barca.png";
+}
+function fourthImage() {
+  myImage.src = "Images/Estadi_Johan_Cruyff.png";
+}
